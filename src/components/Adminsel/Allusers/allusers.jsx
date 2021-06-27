3b147daa308c668e74c85884f673effed81f5a63
Goodbyez <miradadesigual@gmail.com>
2021-06-27 20:26:54 +0200

    Delte User Done

--- src/components/Adminsel/Allusers/allusers.jsx
+++ src/components/Adminsel/Allusers/allusers.jsx
@@ -7,19 +7,34 @@ const Allusers = (props) => {
     const [users, setUsers] = useState([]);
 
     useEffect(()=>{
-        Allusers();
+        allUsers();
     },[]);
 
     useEffect(()=>{
     });
 
-    const Allusers = async () => {
+    const allUsers = async () => {
         try{
             let res = await axios.get('http://localhost:3005/users/allUsers', {headers: {'Authorization': `Basic ${props.logData.token}`}});
             setUsers(res.data)
         } catch (err) {
             console.log({message: err.message})
         }
+    }
+
+    const deleteUser = async (userId) => {
+        
+        try{
+            let body = {
+                "id": userId
+            }
+            console.log("Pasamos por body ",body.coachId)
+            let res = await axios.post('http://localhost:3005/users/delete', body, {headers: {'Authorization': `Basic ${props.logData.token}`}})
+            console.log("este es el resultado", res)
+        } catch (err) {
+            console.log({message: err.message})
+        }
+        allUsers();
     }
 
     if (props.logData.user?.isAdmin){
@@ -29,14 +44,17 @@ const Allusers = (props) => {
                 <div className="usersBox">
                     {users.map((user, index)=>(
                         
-                        <div className="usersCard" key={index}>
-                            <div className="usersData">
+                        <div className="userCard" key={index}>
+                            
+                            <div className="userData">
                                 <div className="usersName">{user.name}</div>
                                 <div className="usersInfo">{user.lastname}</div>
                                 <div className="usersInfo">{user.email}</div>
                                 <div className="usersInfo">{user.country}</div>
                                 <div className="usersInfo">{user.city}</div>
                             </div>
+                            <div className="userButton" onClick={()=>deleteUser(user.id)}>DELETE</div>
+                        
                         </div>
                     ))}
                 </div>
